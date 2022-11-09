c8081b1698318ae9bbeef789960d4b185fce74c1
fergut <gutierrezfdo@gmail.com>
2022-11-09 12:18:54 -0500

    Add test to Comment class

---
+++ src/test/java/com/effynet/blogger/query/domain/entities/CommentTest.java
@@ -0,0 +1,17 @@
+package com.effynet.blogger.query.domain.entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
